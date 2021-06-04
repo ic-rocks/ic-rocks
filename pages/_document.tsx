2c2c9998314c7882051f5664ac3d520644ba6e43
Norton Wang <1502552+FloorLamp@users.noreply.github.com>
2021-06-04 22:19:31 +0700

    Use full url for image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,15 +6,25 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" type="image/png" href="/img/icrocks-logo.png" />
+          <link
+            rel="icon"
+            type="image/png"
+            href="https://ic.rocks/img/icrocks-logo.png"
+          />
           <meta name="description" content={DESCRIPTION} />
           <meta property="og:type" content="website" />
           <meta property="og:description" content={DESCRIPTION} />
-          <meta property="og:image" content="/img/icrocks-hero.png" />
+          <meta
+            property="og:image"
+            content="https://ic.rocks/img/icrocks-hero.png"
+          />
 
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:description" content={DESCRIPTION} />
-          <meta property="twitter:image" content="/img/icrocks-hero.png" />
+          <meta
+            property="twitter:image"
+            content="https://ic.rocks/img/icrocks-hero.png"
+          />
         </Head>
         <body className="dark:bg-gray-900 dark:text-white">
           <script src="/noflash.js" />
